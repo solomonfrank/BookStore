a86ff4252cbd0c2e4ef806dbb383f43717a03dcf
Rock Enwerem <rock@cashboxng.com>
2020-10-24 19:13:03 +0100

    sample custom middleware

--- Startup.cs
+++ Startup.cs
@@ -14,7 +14,7 @@ namespace BookStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
@@ -26,6 +26,12 @@ namespace BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) => {
+
+                await context.Response.WriteAsync("First custom middleware");
+               await next();
+            });
             
             app.UseRouting();
 
