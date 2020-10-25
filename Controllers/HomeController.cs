176138f6c0dc292c9ea7ff1b7f064c8a1d56147e
Rock Enwerem <rock@cashboxng.com>
2020-10-25 18:49:23 +0100

    pass anonymous type to view bag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,12 @@ namespace BookStore.Controllers
     {
         public ViewResult Index()
         {
+            ViewBag.Title = "Home";
+
+            dynamic d = new ExpandoObject();
+            d.Id = 1;
+            d.Name = "jeff frank";
+            ViewBag.Data = d;
             return View();
         }
     }
